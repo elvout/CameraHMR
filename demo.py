ca55006bb904765eb0833a43e200a95ee4a53cfd
Elvin Yang <eyy@umich.edu>
2025-06-30 18:05:25 -0400

    Read all paths from command line

--- demo.py
+++ demo.py
@@ -1,23 +1,35 @@
 import argparse
+import sys
+
 from mesh_estimator import HumanMeshEstimator2
 
 
 def make_parser():
-    parser = argparse.ArgumentParser(description='CameraHMR Regressor')
-    parser.add_argument("--image_folder", "--image_folder", type=str,
-        help="Path to input image folder.")
-    parser.add_argument("--output_folder", "--output_folder", type=str,
-        help="Path to folder output folder.")
+    parser = argparse.ArgumentParser(description="CameraHMR Regressor")
+    parser.add_argument(
+        "--image_folder", "--image_folder", type=str, help="Path to input image folder."
+    )
+    parser.add_argument(
+        "--output_folder",
+        "--output_folder",
+        type=str,
+        help="Path to folder output folder.",
+    )
     return parser
 
+
 def main():
-
-    parser = make_parser()
-    args = parser.parse_args()
+    # parser = make_parser()
+    # args = parser.parse_args()
     estimator = HumanMeshEstimator2()
-    estimator.run_on_video(
-        "Meta_G201_202_Columns_Quickly_round10_rep2_n_view0_us1416148067.mp4"
-    )
+    # estimator.run_on_video(
+    #     "Meta_G201_202_Columns_Tutorial_round1_rep1_tqcs_view0_us107574134.mp4"
+    # )
 
-if __name__=='__main__':
+    for i, path in enumerate(sys.argv[1:]):
+        print(f"{i + 1}/{len(sys.argv) - 1}")
+        estimator.run_on_video(path)
+
+
+if __name__ == "__main__":
     main()
